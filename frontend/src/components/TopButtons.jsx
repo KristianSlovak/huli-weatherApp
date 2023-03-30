755546beea845ce7bf1082e3301cbc39e87642ac
Kristián Slovák <slovak.kristian1@gmail.com>
2023-03-30 12:10:05 +0200

    Deleted toaster and made some small CSS changes

--- frontend/src/components/TopButtons.jsx
+++ frontend/src/components/TopButtons.jsx
@@ -28,13 +28,13 @@ export default function TopButtons({ city, setQuery }) {
   return (
     <div className="flex items-center gap-2">
       <button
-        className="text-white text-lg font-medium"
+        className="text-white text-base md:text-lg font-medium"
         onClick={() => setQuery({ q: city.cityName })}
       >
         {city.cityName}
       </button>
       <button onClick={handleClick}>
-        <UilTrashAlt className="text-white w-5 h-5" />
+        <UilTrashAlt className="text-white w-4 h-4 md:w-5 md:h-5" />
       </button>
     </div>
   );
